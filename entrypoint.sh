6522bc00689c19ed443cec8d15661e8031aafffb
DFP <38843804+diegofernandezpedroche@users.noreply.github.com>
2024-01-26 20:34:03 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -l
 
 echo "Hello $1"   # uses the who-to-greet input variable to print "Hello [who-to-greet]" in the log file
-systemmemory=$(cat /proc/meminfo)      # gets the current time and sets it as an output variable that actions running later in a job can use.
-#echo "::set-output name=time::$time" # (Deprecated) sets the action's output parameter using the workflow syntax
-echo "systemmemory=$systemmemory" >> $GITHUB_OUTPUT
+memory=$(cat /proc/meminfo)      # gets the current time and sets it as an output variable that actions running later in a job can use.
+echo "::set-output name=memory::$memory" # (Deprecated) sets the action's output parameter using the workflow syntax
+#echo "systemmemory=$systemmemory" >> $GITHUB_OUTPUT
