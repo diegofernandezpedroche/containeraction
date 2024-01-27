2674c7147bad59fb010662d567be552aa9323384
DFP <38843804+diegofernandezpedroche@users.noreply.github.com>
2024-01-27 14:43:00 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,7 @@
 echo "Hello $INPUT_MYINPUT"   # uses the who-to-greet input variable to print "Hello [who-to-greet]" in the log file
 memory=$(cat /proc/meminfo)      # gets the current time and sets it as an output variable that actions running later in a job can use.
 #echo "::set-output name=memory::$memory" # (Deprecated) sets the action's output parameter using the workflow syntax
-echo "memory=$memory" >> $GITHUB_OUTPUT
+#echo "memory=$memory" >> $GITHUB_OUTPUT # Error: Unable to process file command 'output' successfully. // because of multilined
+echo 'var<<EOF' >> $GITHUB_OUTPUT
+echo "$memory" >> $GITHUB_OUTPUT
+echo 'EOF' >> $GITHUB_OUTPUT
