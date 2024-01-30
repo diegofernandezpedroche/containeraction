2b4ccebc0fa52b123154194ede07bd8e9772b60c
DFP <38843804+diegofernandezpedroche@users.noreply.github.com>
2024-01-30 08:26:23 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh -l
 
 echo "Hello $INPUT_MYINPUT"   # uses the who-to-greet input variable to print "Hello [who-to-greet]" in the log file
+# done it!
 mymemory=$(cat /proc/meminfo)      # gets the current time and sets it as an output variable that actions running later in a job can use.
 #echo "::set-output name=memory::$memory" # (Deprecated) sets the action's output parameter using the workflow syntax
 #echo "memory=$memory" >> $GITHUB_OUTPUT # Error: Unable to process file command 'output' successfully. // because of multilined
